57e73aced485a3a7afc280193fdcad5cf7ad0e8f
Ben Shelton <ben.shelton@ni.com>
2014-06-10 15:06:50 -0500

    8250: Add Open Firmware support for the two different NI16550 FIFO sizes
    Signed-off-by: Ben Shelton <ben.shelton@ni.com>

--- drivers/tty/serial/of_serial.c
+++ drivers/tty/serial/of_serial.c
@@ -204,6 +204,33 @@ static int of_platform_serial_probe(struct platform_device *ofdev)
 		break;
 	}
 #endif
+#ifdef CONFIG_SERIAL_8250_NI16550
+	case PORT_NI16550_F16:
+	case PORT_NI16550_F128:
+	{
+		struct device_node *np = ofdev->dev.of_node;
+		const char *transceiver;
+		struct uart_8250_port port8250;
+		memset(&port8250, 0, sizeof(port8250));
+		port8250.port = port;
+
+		if (of_property_read_string(np, "transceiver", &transceiver)) {
+			dev_warn(&ofdev->dev, "no transceiver property set\n");
+			return -ENODEV;
+		}
+		if (strcmp(transceiver, "RS-232") == 0) {
+			ret = serial8250_register_8250_port(&port8250);
+		} else if (strcmp(transceiver, "RS-485") == 0) {
+			ret = ni16550_register_port(&port8250);
+		} else {
+			dev_warn(&ofdev->dev,
+				 "unsupported transceiver property (%s)\n",
+				 transceiver);
+			return -EINVAL;
+		}
+		break;
+	}
+#endif
 #ifdef CONFIG_SERIAL_OF_PLATFORM_NWPSERIAL
 	case PORT_NWPSERIAL:
 		ret = nwpserial_register_port(&port);
@@ -240,6 +267,24 @@ static int of_platform_serial_remove(struct platform_device *ofdev)
 	case PORT_8250 ... PORT_MAX_8250:
 		serial8250_unregister_port(info->line);
 		break;
+#endif
+#ifdef CONFIG_SERIAL_8250_NI16550
+	case PORT_NI16550_F16:
+	case PORT_NI16550_F128:
+	{
+		struct device_node *np = ofdev->dev.of_node;
+		const char *transceiver;
+		if (of_property_read_string(np, "transceiver", &transceiver)) {
+			dev_warn(&ofdev->dev, "no transceiver property set\n");
+		} else {
+			if (strcmp(transceiver, "RS-485") == 0) {
+				ni16550_unregister_port(info->line);
+				break;
+			}
+		}
+		serial8250_unregister_port(info->line);
+		break;
+	}
 #endif
 #ifdef CONFIG_SERIAL_OF_PLATFORM_NWPSERIAL
 	case PORT_NWPSERIAL:
@@ -344,6 +389,12 @@ static const struct of_device_id of_platform_serial_table[] = {
 		.data = (void *)PORT_XSCALE, },
 	{ .compatible = "mrvl,pxa-uart",
 		.data = (void *)PORT_XSCALE, },
+#ifdef CONFIG_SERIAL_8250_NI16550
+	{ .compatible = "ni16550-fifo16",
+		.data = (void *)PORT_NI16550_F16, },
+	{ .compatible = "ni16550-fifo128",
+		.data = (void *)PORT_NI16550_F128, },
+#endif
 #ifdef CONFIG_SERIAL_OF_PLATFORM_NWPSERIAL
 	{ .compatible = "ibm,qpace-nwp-serial",
 		.data = (void *)PORT_NWPSERIAL, },
